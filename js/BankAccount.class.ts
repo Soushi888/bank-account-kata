83fb443f40c141c198cd0ec1f514a52b730b7c1b
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-09-21 11:01:12 -0400

    update

--- js/BankAccount.class.ts
+++ js/BankAccount.class.ts
@@ -16,16 +16,24 @@ class BankAccount {
 	}
 
 	deposit = (amount: number) => this.balance += amount;
-	withdraw = (amount: number) => this.balance -= amount;
+
+	withdraw = (amount: number): boolean => {
+		if (amount <= this.balance) {
+			this.balance -= amount;
+			return true;
+		}
+		return false;
+	};
+
 	getAccountBalance = () => {
 		let roundedBalance = Math.round(this.balance * 100) / 100;
 		let decimals: number = parseFloat(roundedBalance.toString().split('.')[1]);
 		if (decimals < 10) decimals *= 10;
-		let strRoundedBalance = roundedBalance.toString().split('.')[0] + '.' + decimals;
+		let strRoundedBalance = roundedBalance.toString().split('.')[0]
+		if (decimals) strRoundedBalance += `.${decimals}`;
 
 		return `The balance of ${this.name}'s account is ${strRoundedBalance}$.`
 	}
-	getHistory = () => this.history;
 
 	private renderHistory = () => {
 		let transactionList: HTMLUListElement = document.querySelector(".transaction-list");
@@ -70,7 +78,8 @@ class BankAccount {
 		let historyBtn: HTMLButtonElement = document.querySelector(".history-btn");
 		let transactionList: HTMLUListElement = document.querySelector(".transaction-list");
 
-		const operationHandler = (operation: string) => {
+		type OperationType = "deposit" | "withdraw" | "history";
+		const operationHandler = (operation: OperationType) => {
 			let amount = amountInput.value;
 			let date = new Date();
 			let formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`;
@@ -94,8 +103,8 @@ class BankAccount {
 						this.history.push(`Deposited ${amount}$ on ${formattedDate}`);
 						break;
 					case "withdraw":
-						this.withdraw(parseFloat(amount));
-						this.history.push(`Withdrew ${amount}$ on ${formattedDate}`);
+						if (this.withdraw(parseFloat(amount)))
+							this.history.push(`Withdrew ${amount}$ on ${formattedDate}`);
 						break;
 				}
 
