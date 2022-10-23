0afc92ef52d4e8146e68f5b1a21b34d05295d7f5
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-10-23 03:19:44 -0400

    transfer method + multi account on a page

--- js/BankAccount.class.ts
+++ js/BankAccount.class.ts
@@ -1,28 +1,44 @@
 class BankAccount {
 	name: string;
 	balance: number;
+	private readonly formattedName;
 	private history: string[] = [];
 	private historyIsShow = false;
+	static accounts: BankAccount[] = [];
 
 	constructor(name: string, balance: number = 0) {
 		this.name = name;
+		this.formattedName = name.trim().toLowerCase()
+			.replace(" ", '-')
+			.replace(/[^a-z0-9\-]/g, '');
 		this.balance = balance;
 
 		if (balance > 0) {
 			this.history.push(`Initial deposit of ${balance}$`);
 		}
 
+		BankAccount.accounts.push(this);
 		this.init();
 	}
 
 	deposit = (amount: number) => this.balance += amount;
 
 	withdraw = (amount: number): boolean => {
-		if (amount <= this.balance) {
-			this.balance -= amount;
-			return true;
-		}
-		return false;
+		if (!this.validateWithdraw(amount)) return false;
+		this.balance -= amount;
+		return true;
+	};
+
+	transfer = (amount: number, receiver_account_name: string): number | boolean => {
+		const receiver_account = BankAccount.accounts.find(account => account.name === receiver_account_name);
+
+		if (!this.validateTransfer(receiver_account_name)) return false;
+
+		this.withdraw(amount);
+		this.history.push(`Transferred ${amount}$ to ${receiver_account_name}`);
+		receiver_account.deposit(amount);
+		receiver_account.history.push(`Received ${amount}$ from ${this.name}`);
+		return this.balance;
 	};
 
 	getAccountBalance = () => {
@@ -35,8 +51,23 @@ class BankAccount {
 		return `The balance of ${this.name}'s account is ${strRoundedBalance}$.`
 	}
 
+	private validateWithdraw = (amount: number) => {
+		if (amount <= this.balance) return true;
+
+		console.error(`Insufficient funds`);
+		return false;
+	}
+
+	private validateTransfer = (receiver_account_name: string) => {
+		if (BankAccount.accounts.find(account => account.name === receiver_account_name)) return true
+
+		console.error(`Account ${receiver_account_name} does not exist`);
+		return false;
+	}
+
 	private renderHistory = () => {
-		let transactionList: HTMLUListElement = document.querySelector(".transaction-list");
+		let transactionList: HTMLUListElement = document.querySelector(`.${this.formattedName}-account .transaction-list`);
+		console.log(transactionList);
 		transactionList.innerHTML = "";
 
 		this.history.forEach(transaction => {
@@ -45,10 +76,10 @@ class BankAccount {
 				case "Initial":
 					className = "initial-transaction";
 					break;
-				case "Deposited":
+				case "Deposited" || "Received":
 					className = "deposit-transaction";
 					break;
-				case "Withdrew":
+				case "Withdrew" || "Transferred":
 					className = "withdraw-transaction";
 					break;
 			}
@@ -58,7 +89,8 @@ class BankAccount {
 	}
 
 	init = () => {
-		document.querySelector(".bank-account").innerHTML = `
+		document.querySelector(".bank-account").innerHTML += `
+			<div class="${this.formattedName}-account">
 				<h2>${this.name}'s account</h2>
 				<p>${this.getAccountBalance()}</p>
 				<div class="amount-input">				
@@ -67,18 +99,22 @@ class BankAccount {
 				<div class="buttons">
 					<button class="deposit-btn">Deposit</button>
 					<button class="withdraw-btn">Withdraw</button>
+					<button class="transfer-btn">Transfer</button>
 					<button class="history-btn">Toggle History</button>
 				</div>
 				<ul class="transaction-list"></ul>
+				</div>
 			`;
 
-		let amountInput: HTMLInputElement = document.querySelector(".amount-input input");
-		let depositBtn: HTMLButtonElement = document.querySelector(".deposit-btn");
-		let withdrawBtn: HTMLButtonElement = document.querySelector(".withdraw-btn");
-		let historyBtn: HTMLButtonElement = document.querySelector(".history-btn");
-		let transactionList: HTMLUListElement = document.querySelector(".transaction-list");
+		let amountInput: HTMLInputElement = document.querySelector(`.${this.formattedName}-account .amount-input input`);
+		let depositBtn: HTMLButtonElement = document.querySelector(`.${this.formattedName}-account .deposit-btn`);
+		let withdrawBtn: HTMLButtonElement = document.querySelector(`.${this.formattedName}-account .withdraw-btn`);
+		let transferBtn: HTMLButtonElement = document.querySelector(`.${this.formattedName}-account .transfer-btn`);
+		let historyBtn: HTMLButtonElement = document.querySelector(`.${this.formattedName}-account .history-btn`);
+		let transactionList: HTMLUListElement = document.querySelector(`.${this.formattedName}-account .transaction-list`);
+		console.log(transferBtn);
 
-		type OperationType = "deposit" | "withdraw" | "history";
+		type OperationType = "deposit" | "withdraw" | "transfer" | "history";
 		const operationHandler = (operation: OperationType) => {
 			let amount = amountInput.value;
 			let date = new Date();
@@ -106,6 +142,10 @@ class BankAccount {
 						if (this.withdraw(parseFloat(amount)))
 							this.history.push(`Withdrew ${amount}$ on ${formattedDate}`);
 						break;
+					case "transfer":
+						let receiverAccountName = prompt("Enter the name of the receiver account");
+						this.transfer(parseFloat(amount), receiverAccountName);
+						break;
 				}
 
 				this.render();
@@ -116,11 +156,12 @@ class BankAccount {
 
 		depositBtn.addEventListener("click", () => operationHandler("deposit"));
 		withdrawBtn.addEventListener("click", () => operationHandler("withdraw"));
+		transferBtn.addEventListener("click", () => operationHandler("transfer"));
 		historyBtn.addEventListener("click", () => operationHandler("history"));
 	}
 
 	render = () => {
-		document.querySelector(".bank-account p").innerHTML = this.getAccountBalance();
+		document.querySelector(`.${this.formattedName}-account p`).innerHTML = this.getAccountBalance();
 		if (this.historyIsShow) {
 			this.renderHistory();
 		}
