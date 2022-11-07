e3aa7488d8689be7a48245f60333d87cf6460b0b
Sacha Pignot <sacha.pignot.partner@decathlon.com>
2022-11-06 23:20:58 -0500

    clean up

--- python/index.py
+++ python/index.py
@@ -1,22 +1,29 @@
+def validate_amount(amount, balance=None, is_balance=False):
+    if amount < 0:
+        raise ValueError(f"{'Balance' if is_balance else 'Amount'} must be positive")
+    if balance is not None and amount > balance:
+        raise ValueError("Amount must be less than balance")
+
+
 class BankAccount:
     def __init__(self, name, balance):
-        self.validate_amount(balance, is_balance=True)
+        validate_amount(balance, is_balance=True)
 
         self.name = name
         self.balance = balance
 
     def deposit(self, amount):
-        self.validate_amount(amount)
+        validate_amount(amount)
         self.balance += amount
         return self.balance
 
     def withdraw(self, amount):
-        self.validate_amount(amount, self.balance)
+        validate_amount(amount, self.balance)
         self.balance -= amount
         return self.balance
 
     def transfer(self, amount, account):
-        self.validate_amount(amount, self.balance)
+        validate_amount(amount, self.balance)
         self.withdraw(amount)
         account.deposit(amount)
         return self.balance
@@ -24,8 +31,3 @@ class BankAccount:
     def __str__(self):
         return f"Account name: {self.name}, Balance: {self.balance}"
 
-    def validate_amount(self, amount, balance=None, is_balance=False):
-        if amount < 0:
-            raise ValueError(f"{'Balance' if is_balance else 'Amount'} must be positive")
-        if balance is not None and amount > balance:
-            raise ValueError("Amount must be less than balance")
